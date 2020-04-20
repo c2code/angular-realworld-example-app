57e1015e48369042195f9da85bbe43680b0ea262
richer ding <rich_dw@126.com>
2020-04-20 13:13:21 +0800

    richer for manager classroom

--- src/app/admin/manager-classroom/manager-classroom.component.ts
+++ src/app/admin/manager-classroom/manager-classroom.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import {MycoursesService} from "../../core/services/mycourses.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UserService} from "../../core/services/user.service";
-import {Course} from "../../core/models/mycourses.module";
+import {Course, Classroom} from "../../core/models/mycourses.module";
 import {User} from "../../core/models/user.model";
+import {ClassroomService} from "../../core/services/classroom.service";
 
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/catch';
@@ -22,11 +23,14 @@ export class ManagerClassroomComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
+    private classroomService:ClassroomService,
     private mycoursesService:MycoursesService
   ) { }
 
   courseList: Course[]
   currentUser: User;
+  classroomList: Classroom[]
+  classroomNum:  number[] = []
 
   ngOnInit() {
 
@@ -40,6 +44,14 @@ export class ManagerClassroomComponent implements OnInit {
 
     //初始化章节课程
     this.populateCourses().subscribe(_ => {;
+      var i: number;
+      for (i=0; i < this.courseList.length;i++) {
+        this.classroomNum[this.courseList[i].cid * 1] = 0;
+        if (this.courseList[i].pid == 0) {
+          this.populateClassrooms(this.courseList[i].cid * 1).subscribe(_ => {;
+          });
+        }
+      }
     });
 
 
@@ -49,6 +61,21 @@ export class ManagerClassroomComponent implements OnInit {
     return this.mycoursesService.getcourses()
       .map((courses) => {
         this.courseList = courses;
+      })
+      .catch((error) => {
+        console.log('error ' + error);
+        throw error;
+      });
+
+  }
+
+  populateClassrooms(cid: number) {
+    return this.classroomService.getclassrooms(cid*1)
+      .map((classrooms) => {
+        this.classroomList = classrooms;
+        if(this.classroomList != null){
+          this.classroomNum[cid] = this.classroomList.length;
+        }
       })
       .catch((error) => {
         console.log('error ' + error);
